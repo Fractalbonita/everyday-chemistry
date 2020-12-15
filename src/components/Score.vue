d5e6f5e6c3b9b09975896fb598d536bff179ef12
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-12-15 16:01:12 +0100

    Add score component

---
+++ src/components/Score.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Score</p>
+    <p>{{ score }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  computed: {
+    score() {
+      return this.$store.state.score;
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
