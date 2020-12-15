ffd24b8eb93753e25c9341cae3ea4368b8f72d94
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-12-15 15:59:12 +0100

    Add basebutton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button :type="type" @click="$emit('click', $event)">
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BaseButton',
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
