fc0a50b5622f5471b6f4b629bf26e7d836a429d4
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-12-15 19:17:06 +0100

    Style button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :type="type" @click="$emit('click', $event)">
+  <button :type="type" @click="$emit('click', $event)" class="button">
     {{ text }}
   </button>
 </template>
@@ -22,4 +22,41 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.button {
+  background-image: linear-gradient(
+    to right,
+    #16a085 0%,
+    #f4d03f 51%,
+    #16a085 100%
+  );
+  background-size: 200% auto;
+  border: none;
+  border-radius: 25px;
+  box-shadow: 0 0 20px #eee;
+  color: white;
+  cursor: pointer;
+  display: block;
+  font-size: 0.875rem;
+  font-weight: 400;
+  letter-spacing: 0.1rem;
+  line-height: 1.5;
+  margin: 0;
+  outline: none;
+  padding: 12px 18px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+
+  &:hover,
+  &:focus {
+    background-position: right center; /* change the direction of the change here */
+    color: #fff;
+    text-decoration: none;
+  }
+  &:active {
+    background-image: none;
+    background-color: #16a085;
+  }
+}
+</style>
