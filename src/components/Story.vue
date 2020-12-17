708dd1a714cb485232053e940d925e078a5fe1fd
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-12-17 01:31:40 +0100

    Enhance button style and add display story time

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -1,16 +1,17 @@
 <template>
-  <div>
-    <progress
+  <div class="container">
+    <BaseProgress
       id="progress"
       :max="stories.length + 1"
       :value="storyIndex + 1"
-    ></progress>
+    />
+    <h2>{{ stories[storyIndex].time }}</h2>
     <p>
       {{ stories[storyIndex].story }}
     </p>
     <h2>{{ stories[storyIndex].question }}</h2>
-    <div v-for="[key, answer] in answers" :key="key">
-      <button type="button" @click="selectAnswer(answer)">
+    <div v-for="[key, answer] in answers" :key="key + storyIndex">
+      <button type="button" @click="selectAnswer(answer)" class="button">
         {{ answer }}
       </button>
     </div>
@@ -20,9 +21,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { mapState } from 'vuex';
+import BaseProgress from './BaseProgress.vue';
 
 export default defineComponent({
   name: 'Story',
+  components: {
+    BaseProgress
+  },
   computed: {
     ...mapState(['answer', 'storyIndex', 'stories']),
     answers(): [string, string][] {
@@ -47,4 +52,44 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.button {
+  background-image: linear-gradient(
+    to right,
+    var(--color-primary) 0%,
+    var(--color-secondary) 51%,
+    var(--color-primary) 100%
+  );
+  background-size: 200% auto;
+  border: none;
+  border-radius: 25px;
+  box-shadow: 0 0 20px var(--color-box-shadow);
+  color: var(--color-surface);
+  cursor: pointer;
+  display: block;
+  font-size: var(--font-size-body);
+  font-weight: 500;
+  letter-spacing: 0.1rem;
+  line-height: 1.5;
+  margin: 0.5rem 0;
+  outline: none;
+  padding: 12px 18px;
+  text-align: center;
+  transition: 0.5s;
+
+  &:hover,
+  &:focus {
+    background-position: right center;
+    color: var(--color-surface);
+  }
+  &:active {
+    background-image: none;
+    background-color: var(--color-primary);
+  }
+}
+</style>
