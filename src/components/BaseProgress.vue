2fe620ef1e95a9bbaf7131e91ad3077a6cbc009b
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-12-17 01:30:47 +0100

    Add baseprogress component

---
+++ src/components/BaseProgress.vue
@@ -0,0 +1,27 @@
+<template>
+  <progress :id="id" :max="name" :value="value"></progress>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BaseProgress',
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: [Number, String],
+      required: true
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
