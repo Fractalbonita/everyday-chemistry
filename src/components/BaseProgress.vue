932d54ce3e18fb4d0696a615ea94a505442bab6c
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-12-17 16:59:11 +0100

    Style progress bar

--- src/components/BaseProgress.vue
+++ src/components/BaseProgress.vue
@@ -1,5 +1,5 @@
 <template>
-  <progress :id="id" :max="max" :value="value"></progress>
+  <progress :id="id" :max="max" :value="value" class="progress"></progress>
 </template>
 
 <script lang="ts">
@@ -24,4 +24,55 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.progress {
+  background-color: var(--color-input);
+  border: none;
+  border-radius: 25px;
+  box-shadow: 0 1px 5px var(--color-background) inset;
+
+  &[value] {
+    -webkit-appearance: none;
+    appearance: none;
+    height: 15px;
+    width: 185px;
+
+    &::-webkit-progress-bar {
+      background-color: var(--color-input);
+      border: none;
+      border-radius: 25px;
+      box-shadow: 0 1px 5px var(--color-background) inset;
+    }
+    &::-webkit-progress-value {
+      background-color: var(--color-primary);
+      background-image: -webkit-linear-gradient(
+        left,
+        var(--color-primary) 0%,
+        var(--color-secondary) 51%,
+        var(--color-primary) 100%
+      );
+      background-size: 200% auto;
+      border-top-left-radius: 25px;
+      border-bottom-left-radius: 25px;
+      border-top-right-radius: 25px;
+      border-bottom-right-radius: 25px;
+      box-shadow: 0 0 4px var(--color-background) inset;
+    }
+    &::-moz-progress-bar {
+      background-color: var(--color-primary);
+      background-image: -moz-linear-gradient(
+        left,
+        var(--color-primary) 0%,
+        var(--color-secondary) 51%,
+        var(--color-primary) 100%
+      );
+      background-size: 200% auto;
+      border-top-left-radius: 25px;
+      border-bottom-left-radius: 25px;
+      border-top-right-radius: 25px;
+      border-bottom-right-radius: 25px;
+      box-shadow: 0 0 4px var(--color-background) inset;
+    }
+  }
+}
+</style>
