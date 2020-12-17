37de72ee2a3d698eaad71ebe5fb79f49670d48f9
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-12-17 01:29:30 +0100

    Enhance style and add variables

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,15 +1,17 @@
 <template>
   <label :for="id" class="label">{{ label }}</label>
-  <input
-    v-bind="$attrs"
-    :id="id"
-    :name="name"
-    type="text"
-    autocomplete="off"
-    :placeholder="placeholder"
-    v-model="value"
-    class="input"
-  />
+  <span class="effect">
+    <input
+      v-bind="$attrs"
+      :id="id"
+      :name="name"
+      type="text"
+      autocomplete="off"
+      :placeholder="placeholder"
+      v-model="value"
+      class="input"
+    />
+  </span>
 </template>
 
 <script lang="ts">
@@ -64,31 +66,54 @@ export default defineComponent({
   position: absolute;
   width: 1px;
 }
+.effect {
+  display: block;
+  margin: 0.5rem auto;
+  position: relative;
+  width: 60%;
+
+  &::before {
+    content: ' ';
+    background-image: url('../assets/images/cauldron.jpg');
+    background-attachment: fixed;
+    background-position: center;
+    background-size: cover;
+    border: 1px solid var(--color-box-shadow);
+    border-radius: 25px;
+    filter: blur(10px);
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+  }
+}
 .input {
-  background-color: white;
-  border: 1px solid black;
+  background-color: var(--color-input);
+  border: none;
   border-radius: 25px;
-  color: black;
+  color: var(--color-background);
   cursor: text;
-  font-size: 16px;
+  font-size: var(--font-size-body);
   font-weight: 400;
   height: 48px;
   outline: none;
   padding: 0 12px;
-  width: 80%;
+  width: 100%;
 
   &::placeholder {
-    color: black;
-    font-size: 0.875rem;
-    font-weight: normal;
+    color: var(--color-background);
+    font-size: var(--font-size-button);
+    font-weight: 400;
     letter-spacing: 0.1rem;
-    opacity: 0.5;
+    opacity: 0.7;
   }
   &:hover {
-    border: 1px solid #16a085;
+    border: 1px solid var(--color-surface);
   }
   &:focus {
-    border: 2px solid #16a085;
+    border: 2px solid var(--color-surface);
   }
 }
 </style>
