492ef7372a7e7831e607f2bc62b279b794ea0c63
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-12-20 23:22:59 +0100

    Add store states to interface

--- src/shims-vuex.d.ts
+++ src/shims-vuex.d.ts
@@ -3,8 +3,12 @@ import { Store } from 'vuex';
 
 declare module '@vue/runtime-core' {
   interface State {
+    uiState: string;
+    player: string;
     sidekick: string;
+    sidekickOptions: array;
     score: number;
+    answer: string;
     answers: { [key: string]: string };
     storyIndex: number;
     stories: unknown[];
