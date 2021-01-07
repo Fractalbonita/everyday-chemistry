2d72e6282a2260b7af8afec365fb10be78bb9739
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2021-01-07 16:56:57 +0100

    Update config files

---
+++ vue.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:8080/',
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+        @import "~@/assets/styles/_variables.scss";
+        `
+      }
+    }
+  }
+};
